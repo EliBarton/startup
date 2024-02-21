3433d71b46be46fa76ba061d45853b20d5ee5c78
EliBarton <70331034+EliBarton@users.noreply.github.com>
2024-02-20 22:24:53 -0700

    Got username displaying on the welcome page

---
+++ login.js
@@ -0,0 +1,18 @@
+function login() {
+  const nameEl = document.querySelector("#username");
+  localStorage.setItem("userName", nameEl.value);
+  window.location.href = "play.html";
+}
+
+function getPlayerName() {
+  return localStorage.getItem('userName') ?? false;
+}
+let playerNameEl = document.querySelector("#playername");
+if (getPlayerName()){
+  
+  playerNameEl.innerHTML = getPlayerName();
+  console.log(playerNameEl);
+}
+else{
+  playerNameEl.outerHTML = `<p class="nav" style="font-size: large; color: gray" id="playername">Not Logged In</p>`
+}
