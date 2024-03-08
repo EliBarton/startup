79afda00348524bc7ba69b2bf805eba15e811255
EliBarton <70331034+EliBarton@users.noreply.github.com>
2024-03-08 15:31:53 -0700

    Attempting to fix score saving

--- public/scores.js
+++ public/scores.js
@@ -1,11 +1,35 @@
 getBackgroundImage();
-let localScore = 0;
-updateLocalScore(0);
+loadScores();
 updateScoreEl();
 getLocalScore();
 
 let interval = setInterval(getLocalScore, 3000);
 
+
+async function loadScores() {
+let scores = [];
+players = [];
+    try {
+        // Get the latest high scores from the service
+        const response = await fetch('/api/scores');
+        scores = await response.json();
+        // Add the scores to the array of players for easy access
+        scores.forEach(player => {
+            players.push([player.name, player.score])
+        });
+        // Save the scores in case we go offline in the future
+        localStorage.setItem('scores', JSON.stringify(players));
+    } catch {
+        // If there was an error then just use the last saved scores
+        console.log("Error getting scores");
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+            scoresText.forEach(player => {
+                players.push([player.name, player.score])
+            });
+        }
+    }
+}
 
 function getLocalScore(){
     let openDB = indexedDB.open("/userfs", 21);
@@ -45,9 +69,7 @@ async function saveScore(score) {
         // Save the scores in case we go offline in the future
         let players = [];
         scores.forEach(player => {
-            if (player.name != getPlayerName()){
-                players.push([player.name, player.score])
-            }
+            players.push([player.name, player.score])
         });
         localStorage.setItem('scores', JSON.stringify(players));
         // Set the high score element to the new high score
