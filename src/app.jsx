1e034144a1cc94d8deae5266c413e7f7eabfcb86
EliBarton <70331034+EliBarton@users.noreply.github.com>
2024-04-06 23:03:04 -0600

    Added Browser Router

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
-import { BrowserRouter } from 'react-router-dom';
+import './style.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Scores } from './scores/scores';
+import { Play } from './play/play';
 
 export default function App() {
   return <BrowserRouter>
-  <div className="bg-dark text-light">
+  <div className="bg-dark text-light app">
   <header className="container-fluid">
       <nav className="navbar navbar-dark">
           <p className="navbar-brand">GalagaOnline</p>
-      <menu className="navbar-nav ">
-              <li className="nav-item"><a className="nav-link active" href="index.html">Welcome</a></li>
-              <li className="nav-item" id="playControls"><a className="nav-link" href="play.html">Play</a></li>
-              <li className="nav-item" id="playControls"><a className="nav-link" href="scores.html">Scores</a></li>
+      <menu className="navbar-nav">
+              <li className="nav-item"><NavLink className="nav-link active" to="index">Welcome</NavLink></li>
+              <li className="nav-item" id="playControls"><NavLink className="nav-link" to="play">Play</NavLink></li>
+              <li className="nav-item" id="playControls"><NavLink className="nav-link" to="scores">Scores</NavLink></li>
       </menu>
-      <p className="nav" style="font-size: large;" id="playername">Username</p>
+      <p className="nav" id="playername">Username</p>
   </nav>
   </header>
-  <main className="container-fluid text-center">
-      App components go here!
-  </main>
+
+  <Routes>
+  <Route path='/' element={<Login />} exact />
+  <Route path='/play' element={<Play />} />
+  <Route path='/scores' element={<Scores />} />
+  <Route path='*' element={<NotFound />} />
+</Routes>
 
 <footer>
   <span>Creator: Eli Barton</span>
